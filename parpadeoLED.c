f62e9d46332ba8902d5903daa0f259694c8739c7
Lab. Sistemas Embebidos <lab-microcon@fiec.espol.edu.ec>
2025-09-30 20:53:46 -0500

    Create parpadeoLED.c

---
+++ parpadeoLED.c
@@ -0,0 +1,32 @@
+#include <Arduino.h> 
+#include <avr/io.h>
+#include <util/delay.h> 
+
+int led0 = 0;
+int led1 = 1;
+int led2 = 2;
+
+int main()
+ { 
+    DDRB = 1 << led0;
+    DDRB |= 1 << led1;
+    DDRB |= 1 << led2;
+ //DDRB = (1 << led0) | (1 << led1) | (1 << led2);
+ //DDRB = 0B00000111; //0B111; //NUMERO EN BINARIO
+ //DDRB = 0X07; //NUMERO EN HEXADECIMAL
+ //DDRB = 7; //NUMERO EN DECIMAL
+   
+    while (1){
+       
+       PORTB = 1<<led0;
+       _delay_ms(1000);
+       PORTB |= 1<<led1;
+       _delay_ms(1000);
+       PORTB |= 1<<led2;
+       _delay_ms(1000);
+       PORTB &= ~(1<<led2) & ~(1<<led1) & ~(1<<led0);
+       _delay_ms(1000);
+       }
+ return 0;
+       
+ }
