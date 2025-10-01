2fcc82f77da90c30444c8e3a1f29420a66a6551c
Lab. Sistemas Embebidos <lab-microcon@fiec.espol.edu.ec>
2025-09-30 20:56:22 -0500

    Create stepper1.c

---
+++ stepper1.c
@@ -0,0 +1,68 @@
+#include <avr/io.h> 
+#include <avr/io.h>
+#include <avr/sleep.h>
+#include <util/delay.h>
+
+uint8_t p1 = 0;
+uint8_t p2 = 1;
+uint8_t p3 = 2;
+uint8_t p4 = 3;
+//Tiempo necesario para energizar cada bobina
+//En el simulador proteus con 500ms funciona
+uint16_t demora = 500;  //5 para motor físico
+
+int main()
+{ 
+    DDRB = (1 << p1) | (1 << p2) | (1 << p3) | (1 << p4); //pines 0,1,2,3 del puerto B como salida
+    while (1)
+      {
+
+	    PORTB |= (1 << p1);
+	    PORTB &= ~(1 << p2);
+	    PORTB &= ~(1 << p3);
+	    PORTB &= ~(1 << p4);
+	    _delay_ms (demora);
+	    
+	    PORTB |= (1 << p1);
+	    PORTB |= (1 << p2);
+	    PORTB &= ~(1 << p3);
+	    PORTB &= ~(1 << p4);
+	    _delay_ms (demora);
+	    
+	    PORTB &= ~(1 << p1);
+	    PORTB |= (1 << p2);
+	    PORTB &= ~(1 << p3);
+	    PORTB &= ~(1 << p4);
+	    _delay_ms (demora);
+	    
+	    PORTB &= ~(1 << p1);
+	    PORTB |= (1 << p2);
+	    PORTB |= (1 << p3);
+	    PORTB &= ~(1 << p4);
+	    _delay_ms (demora);
+	    
+	    PORTB &= ~(1 << p1);
+	    PORTB &= ~(1 << p2);
+	    PORTB |= (1 << p3);
+	    PORTB &= ~(1 << p4);
+	    _delay_ms (demora);
+	    
+	    PORTB &= ~(1 << p1);
+	    PORTB &= ~(1 << p2);
+	    PORTB |= (1 << p3);
+	    PORTB |= (1 << p4);
+	    _delay_ms (demora);
+	    
+	    PORTB &= ~(1 << p1);
+	    PORTB &= ~(1 << p2);
+	    PORTB &= ~(1 << p3);
+	    PORTB |= (1 << p4);
+	    _delay_ms (demora);
+	    
+     	PORTB |= (1 << p1);
+	    PORTB &= ~(1 << p2);
+	    PORTB &= ~(1 << p3);
+	    PORTB |= (1 << p4);
+	    _delay_ms (demora);
+      }     
+}
